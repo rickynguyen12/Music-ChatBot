3ff14ea953d34238f14f7753804a1200613d885a
rickynguyen12 <Rricky612@gmail.com>
2025-07-14 17:59:42 -0700

    Deploy files

--- backend/app.py
+++ backend/app.py
@@ -109,6 +109,7 @@ def recommend():
 
     return jsonify(suggestions)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
 
